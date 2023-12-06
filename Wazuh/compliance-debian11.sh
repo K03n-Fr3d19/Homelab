084b3338b471951a002d222f8889681fcdba1132
Koenfred19 <143009216+Koenfred19@users.noreply.github.com>
2023-12-06 20:05:18 +0100

    Update compliance-debian11.sh

--- Wazuh/compliance-debian11.sh
+++ Wazuh/compliance-debian11.sh
@@ -22,18 +22,18 @@ apt purge talk
 apt install aide aide-common
 mv /var/lib/aide/aide.db.new /var/lib/aide/aide.db
 ### INSTALL SUDO FOR PERMISSION MANAGEMENT ###
-apt install sudo
+apt install sudo -y
 ### SET PERMISSIONS FOR SHADOW ###
 chown root:shadow /etc/shadow && chmod u-x,g-wx,o-rwx /etc/shadow
 chown root:shadow /etc/shadow- && chmod u-x,g-wx,o-rwx /etc/shadow-
 chown root:shadow /etc/gshadow && chmod u-x,g-wx,o-rwx /etc/gshadow
 chown root:shadow /etc/gshadow- && chmod u-x,g-wx,o-rwx /etc/gshadow-
 ### INSTALL AppArmor ###
-apt install apparmor apparmor-utils
+apt install apparmor apparmor-utils -y
 ### INSTALL UFW (Uncomplicated Firewall) ###
-apt install ufw
+apt install ufw -y
 ### INSTALL AUDITD ###
-apt install auditd audispd-plugins
+apt install auditd audispd-plugins -y
 systemctl --now enable auditd
 ### USERS THAT ARE INACTIVE FOR 30 DAYS OR MORE ARE AUTMATICLY DISABLED ###
 useradd -D -f 30
