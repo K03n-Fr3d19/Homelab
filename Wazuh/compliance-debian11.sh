c84c76ec6a086d61a5dd78b600f0ee022165459d
Koenfred19 <143009216+Koenfred19@users.noreply.github.com>
2023-12-06 20:04:15 +0100

    Update compliance-debian11.sh

--- Wazuh/compliance-debian11.sh
+++ Wazuh/compliance-debian11.sh
@@ -1,6 +1,10 @@
 #
 #   CIS Debian Linux 11 Benchmark v1.0.0
 #
+### UPDATES ANY OUTDATED PACKAGES ###
+apt update
+apt -s upgrade
+apt upgrade -y
 ### CRON PERMISSIONS ###
 chown root:root /etc/cron.d/ && chmod og-rwx /etc/cron.d/
 chown root:root /etc/crontab && chmod og-rwx /etc/crontab
@@ -8,9 +12,15 @@ chown root:root /etc/cron.hourly/ && chmod og-rwx /etc/cron.hourly/
 chown root:root /etc/cron.daily/ && chmod og-rwx /etc/cron.daily/
 chown root:root /etc/cron.weekly/ && chown root:root /etc/cron.weekly/
 chown root:root /etc/cron.monthly/ && chmod og-rwx /etc/cron.monthly/
+### SET PERMISSIONS OF BOOTLOADER (GRUB) ###
+chown root:root /boot/grub/grub.cfg
+chmod u-wx,go-rwx /boot/grub/grub.cfg
 ### DISABLE DANGEROUS SERVICES ###
 apt purge telnet
 apt purge talk
+### INSTALL & SETUP AIDE ###
+apt install aide aide-common
+mv /var/lib/aide/aide.db.new /var/lib/aide/aide.db
 ### INSTALL SUDO FOR PERMISSION MANAGEMENT ###
 apt install sudo
 ### SET PERMISSIONS FOR SHADOW ###
@@ -18,3 +28,12 @@ chown root:shadow /etc/shadow && chmod u-x,g-wx,o-rwx /etc/shadow
 chown root:shadow /etc/shadow- && chmod u-x,g-wx,o-rwx /etc/shadow-
 chown root:shadow /etc/gshadow && chmod u-x,g-wx,o-rwx /etc/gshadow
 chown root:shadow /etc/gshadow- && chmod u-x,g-wx,o-rwx /etc/gshadow-
+### INSTALL AppArmor ###
+apt install apparmor apparmor-utils
+### INSTALL UFW (Uncomplicated Firewall) ###
+apt install ufw
+### INSTALL AUDITD ###
+apt install auditd audispd-plugins
+systemctl --now enable auditd
+### USERS THAT ARE INACTIVE FOR 30 DAYS OR MORE ARE AUTMATICLY DISABLED ###
+useradd -D -f 30
